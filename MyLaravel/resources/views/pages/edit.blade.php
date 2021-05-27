2fa8c991f83976c08e41ddf15106b16b46c32078
Diego Barbaresco <diego.bar@live.it>
2021-05-27 18:49:28 +0200

    Correzione form

--- MyLaravel/resources/views/pages/edit.blade.php
+++ MyLaravel/resources/views/pages/edit.blade.php
@@ -33,5 +33,15 @@
                 </button>
             </div>
         </div>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </form>
 @endsection
